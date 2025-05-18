19f4fe488d408b741bb26a3efc44a40cbcde063b
lyyQwQ <53158373+lyyQwQ@users.noreply.github.com>
2025-05-18 11:23:46 +0800

    Fix edge-tts import and add UI

---
+++ src/server/services/tts/services/edge.ts
@@ -0,0 +1,46 @@
+import { tts } from "edge-tts";
+import { isEmptyValue } from "../../../../utils";
+import { TTS_State } from "../schema";
+import { ITTSReceiver, ITTSService } from "../types";
+
+export class TTS_EdgeService implements ITTSService {
+  constructor(private bindings: ITTSReceiver) {}
+
+  dispose(): void {}
+
+  get state() {
+    return window.ApiServer.state.services.tts.data.edge;
+  }
+
+  start(state: TTS_State): void {
+    if (Object.values(this.state).some(isEmptyValue))
+      return this.bindings.onStop("Options missing");
+    this.bindings.onStart();
+  }
+
+  async play(value: string) {
+    try {
+      const buffer = (await tts(value, {
+        voice: this.state.voice,
+        rate: this.state.rate,
+        volume: this.state.volume,
+      })) as unknown as Uint8Array;
+
+      const data =
+        buffer instanceof Uint8Array
+          ? buffer
+          : new Uint8Array(buffer as ArrayBuffer);
+      window.ApiServer.sound.enqueueVoiceClip(data.buffer, {
+        volume: parseFloat(this.state.volume) ?? 1,
+        rate: parseFloat(this.state.rate) ?? 1,
+        device_name: this.state.device,
+      });
+    } catch (e: any) {
+      this.bindings.onStop(e?.message);
+    }
+  }
+
+  stop(): void {
+    this.bindings.onStop();
+  }
+}
